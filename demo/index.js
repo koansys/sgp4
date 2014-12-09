ab12f07f3e502f50fde45d4a60005bee2cc2cdfb
Reed O'Brien <reed@reedobrien.com>
2014-12-09 13:16:27 -0500

    Add tle files, and start adding cesium demo.
    Refs #7

---
+++ demo/index.js
@@ -0,0 +1,16 @@
+// Copyright 2014 Reed O'Brien <reed@koansys.com>, Chris Shenton
+// <chris@koansys.com>, and Contributors.
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+'use strict';
+
+requirejs.config({
+    baseUrl: "../lib",
+
+    paths: {
+        cesium: '/demo/cesium/Source'
+    }
+});
+
+// Need to setup a cesium requirejs loader.
